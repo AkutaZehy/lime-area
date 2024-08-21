17e98b813505b4e39c714970cf4ccb6f1df0f127
Akuta Zehy <akuta_zehy@outlook.com>
2024-08-22 01:04:31 +0800

    feat(footer): adaptive footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,14 @@
+<script setup>
+	const currentYear = new Date().getFullYear();
+	const yearsSince2018 = Math.ceil((currentYear - 2018));
+</script>
+
 <template>
 	<footer>
 		<p color="grey">Still Under Development.</p>
 		<p>
-			©2018-2024 Akuta Zehy，Some Rights Reserved <br />
-			so 2018 is 6 years ago bruh
+			©2018-{{ currentYear }}, Akuta Zehy，Some Rights Reserved <br />
+			so 2018 is {{ yearsSince2018 }} years ago bruh
 		</p>
 	</footer>
 </template>
